270f93ea2cfd48bdd3648616b3b4633e845a7d47
Fabricio Borgobello <fborgobello@gmail.com>
2021-01-20 20:45:42 -0300

    Server side rendering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
 import "./styles/App.scss";
+import React from "react";
+import { MemoryRouter, Route, Link } from "react-router-dom";
 
-const App = () => {
+const Home = () => (
+  <>
+    <h1>Home</h1>
+    <ul>
+      <li key="1">
+        <Link to="/first-page">First Page</Link>
+      </li>
+      <li key="2">
+        <Link to="/second-page">Second Page</Link>
+      </li>
+    </ul>
+  </>
+);
+
+const FirstPage = () => (
+  <>
+    <h1>First Page</h1>
+    <Link to="/">Go back Home</Link>
+  </>
+);
+
+const SecondPage = () => (
+  <>
+    <h1>Second Page</h1>
+    <Link to="/">Go back Home</Link>
+  </>
+);
+
+function App() {
   return (
-    <div>
-      <h1>Hola</h1>
-    </div>
+    <MemoryRouter>
+      <Route exact path="/" component={Home} />
+      <Route path="/first-page" component={FirstPage} />
+      <Route path="/second-page" component={SecondPage} />
+    </MemoryRouter>
   );
-};
+}
 
 export default App;

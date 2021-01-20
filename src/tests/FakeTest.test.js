a68aeef1dc5c1bb2ed44a549eca8670b13b8f6ed
Fabricio Borgobello <fborgobello@gmail.com>
2021-01-20 18:30:00 -0300

    First commit

---
+++ src/tests/FakeTest.test.js
@@ -0,0 +1,8 @@
+describe("Test for prevent errors", () => {
+  it("Test that 2 + 2 = 4", () => {
+    const result = 2 + 2;
+    const expected = 4;
+
+    expect(result).toBe(expected);
+  });
+});

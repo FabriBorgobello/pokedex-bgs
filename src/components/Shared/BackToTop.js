fe634013ea3d6ebe37b971b4c778cac9ae2a50de
Fabricio Borgobello <fborgobello@gmail.com>
2021-01-23 16:26:07 -0300

    Fixed bug on BackToTop button

--- src/components/Shared/BackToTop.js
+++ src/components/Shared/BackToTop.js
@@ -2,29 +2,29 @@ import * as React from "react";
 import styles from "./BackToTop.module.scss";
 
 const BackToTop = () => {
-    const [showScroll, setShowScroll] = React.useState(false);
-    const checkScrollTop = () => {
-        if (!showScroll && window.pageYOffset > 400) {
-            setShowScroll(true);
-        } else if (showScroll && window.pageYOffset <= 400) {
-            setShowScroll(false);
-        }
-    };
-    window.addEventListener("scroll", checkScrollTop);
+  const [showScroll, setShowScroll] = React.useState(false);
+  const checkScrollTop = () => {
+    if (!showScroll && window.pageYOffset > 400) {
+      setShowScroll(true);
+    } else if (showScroll && window.pageYOffset <= 400) {
+      setShowScroll(false);
+    }
+  };
+  window.addEventListener("scroll", checkScrollTop);
 
-    const scrollTop = () => {
-        window.scrollTo({ top: 0, behavior: "smooth" });
-    };
+  const scrollTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
 
-    return (
-        <div
-            className={styles.FabContainer}
-            onClick={scrollTop}
-            style={{ height: 40, display: showScroll ? "flex" : "none" }}
-        >
-            <i className={`fas fa-arrow-up scrollTop ${styles.FabIcon}`} />
-        </div>
-    );
+  return (
+    <div
+      className={styles.FabContainer}
+      onClick={scrollTop}
+      style={{ height: 40, display: showScroll ? "flex" : "none" }}
+    >
+      <i className={`fas fa-arrow-up scrollTop ${styles.FabIcon}`} />
+    </div>
+  );
 };
 
 export default BackToTop;
